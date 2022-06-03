93adfb9f2f3a19f3de17ce0deb493e163aa4703a
Jagriti <jagriti.agni@gmail.com>
2022-06-03 14:42:13 +0200

    first test case updated

--- test_visualize.py
+++ test_visualize.py
@@ -5,8 +5,8 @@ import pandas as pd
 import visualize
 
 def test_visualize_eda():
-    test_input_path = './data/train.csv'
-    result = brane_visualize.visualize_EDA(test_input_path)
+    test_input_path = '/data/train.csv'
+    result = visualize.visualize_EDA(test_input_path)
     expected_output = 0
     assert  expected_output == result
 

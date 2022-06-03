3ba3aa938c8d18b413d3382516f72ea53394bc2f
Jagriti <jagriti.agni@gmail.com>
2022-06-03 20:37:19 +0200

    Test case updated

--- test_visualize.py
+++ test_visualize.py
@@ -14,12 +14,6 @@ def test_get_model():
     model_name = 'dtc'
     result = visualize.get_model(model_name)
     expect_model = DecisionTreeClassifier()
-    assert result == expect_model
-
-def test_visualize_eda():
-    test_input_path = './data/train.csv'
-    result = visualize.visualize_EDA(test_input_path)
-    expected_output = 0
-    assert  expected_output == result
+    assert type(result) == type(expect_model)
 
 
